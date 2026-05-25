2a4580732203ad0047e3ce9144e675d3f9a670d5
michaelamaart-svg <michaelamaart@gmail.com>
2026-05-25 19:54:59 +0200

    Add files via upload

---
+++ conditionals.js
@@ -0,0 +1,17 @@
+// Create a variable called score and give it a value between 0 and 100
+let score = 75;
+
+// Create a variable called passingGrade and set it to 60
+let passingGrade = 60;
+
+// Check if score is greater than or equal to passingGrade
+if (score >= passingGrade) {
+    console.log("You have passed");
+} else {
+    console.log("You have not passed");
+}
+
+// Check if the score is perfect
+if (score === 100) {
+    console.log("You have a perfect score");
+}
